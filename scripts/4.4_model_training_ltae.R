8e4a11a1c28ab72dec9413cbaef694a1f627ab21
Miguel Cunha <wolfherz@tutamail.com>
2026-05-05 10:53:46 -0300

    Minor bug fixes

--- scripts/4.4_model_training_ltae.R
+++ scripts/4.4_model_training_ltae.R
@@ -1,5 +1,5 @@
 # ============================================================
-# Train a Light Temporal Attention Encoder deep learning model
+# Train a Lightweight Temporal Self-Attention Encoder
 # ============================================================
 
 # Load required libraries
@@ -7,12 +7,15 @@ library(sits)
 library(ggplot2)
 library(torch)
 library(luz)
+library(stringr)
 
 # Define the parameters: These are user-defined variables
-time_series_name  <- "TS-tiles_012014-012015-013014-013015_1y_2024-08-01_2025-07-31_all-samples-new-pol-avg-false_2026-04-22_10h46m.rds"
-start_date        <- "2024-08-01"
-end_date          <- "2025-07-31"
-tiles             <- c("012014","012015","013014","013015")
+time_series_name  <- "TS-tiles_012014-012015-013014-013015_1y_2024-08-01_2025-07-31_all-samples-new-pol-avg-false_2026-02-24_20h01m.rds"
+
+# Extract the tiles and date of the string separated by "_"
+tiles      <- str_split(str_extract(time_series_name, "(?<=tiles_)[^_]+"), "-")[[1]]
+start_date <- stringr::str_split_i(time_series_name, "_", 4)
+end_date   <- stringr::str_split_i(time_series_name, "_", 5)
 
 # Function to read class names and their colors::IMPORTANT
 read_class_config <- function(config_file = "class_config.txt") {
@@ -77,6 +80,10 @@ config_dir        <- ".."
 # Identifier to distinguish this model run from previous versions
 var <- stringr::str_split_i(time_series_name, "_", 6)
 
+# Plots organized by var
+plots_dir <- file.path(plots_path, var)
+dir.create(plots_dir, showWarnings = FALSE, recursive = TRUE)
+
 # ============================================================
 # 1. Define and Load Data Cubes
 # ============================================================
@@ -118,7 +125,9 @@ rfor_validate <- sits_kfold_validate(
   progress = TRUE) # adapt to your computer CPU core availability
 sits_kfold_validate_end <- Sys.time()
 sits_kfold_validate_time <- as.numeric(sits_kfold_validate_end - sits_kfold_validate_start, units = "secs")
-sprintf("SITS kfold_validate process duration (HH:MM): %02d:%02d", as.integer(sits_kfold_validate_time / 3600), as.integer((sits_kfold_validate_time %% 3600) / 60))
+sprintf("SITS kfold_validate process duration (HH:MM): %02d:%02d", 
+        as.integer(sits_kfold_validate_time / 3600), 
+        as.integer((sits_kfold_validate_time %% 3600) / 60))
 
 # Step 2.3.1 -- Plot the confusion matrix
 plot(rfor_validate, type = "confusion_matrix")
@@ -126,40 +135,66 @@ plot(rfor_validate, type = "confusion_matrix")
 # Step 2.3.2 -- Plot the metrics by class
 plot(rfor_validate, type = "metrics")
 
+# Step 2.4 -- Save confusion matrix plot
+g_cm <- plot(rfor_validate, type = "confusion_matrix")
+ggplot2::ggsave(
+  filename = file.path(
+    plots_dir,
+    paste0(
+      "Kfold-confusion-matrix_",
+      tiles_train, "_",
+      start_date, "_", end_date, "_",
+      var, "_",
+      format(Sys.Date(), "%Y-%m-%d"),
+      ".png"
+    )
+  ),
+  plot = g_cm,
+  width = 1600,
+  height = 1000,
+  units = "px",
+  dpi = 200
+)
+
+# Step 2.4.1 -- Save metrics plot
+g_metrics <- plot(rfor_validate, type = "metrics")
+ggplot2::ggsave(
+  filename = file.path(
+    plots_dir,
+    paste0(
+      "Kfold-metrics_",
+      tiles_train, "_",
+      start_date, "_", end_date, "_",
+      var, "_",
+      format(Sys.Date(), "%Y-%m-%d"),
+      ".png"
+    )
+  ),
+  plot = g_metrics,
+  width = 1600,
+  height = 1000,
+  units = "px",
+  dpi = 200
+)
+
 # ============================================================
 # 3. Training and saving model
 # ============================================================
 
-# Step 3.1 -- Perform hyperparameter tuning for TempCNN-based model
-tuned_ltae <- sits_tuning(
+# Step 3.1 -- Train LTAE-based model using training samples
+tuned_ltae <- sits_lighttae(
   samples = train_samples,
-  ml_method = sits_tempcnn(),
+  epochs = 150L,
+  batch_size = 128L,
   validation_split = 0.2,
-  params = sits_tuning_hparams(
-    # Convolutional architecture options
-    cnn_layers = choice(
-      c(256,256,256), 
-      c(128,128,128), 
-      c(64,64,64)
-    ),  # number of filters per layer
-    cnn_kernels = choice(
-      c(3,3,3), 
-      c(5,5,5), 
-      c(7,7,7)
-    ),  # kernel sizes
-    cnn_dropout_rates = choice(
-      c(0.2,0.2,0.2), 
-      c(0.3,0.3,0.3), 
-      c(0.4,0.4,0.4)
-    ),  # dropout rates for regularization
-    # Optimizer configuration (AdamW)
-    optimizer = torch::optim_adamw,
-    opt_hparams = list(
-      lr = loguniform(1e-2, 1e-4),        # learning rate (log-scale search)
-      weight_decay = loguniform(1e-2, 1e-8) # L2 regularization strength
-    )
-  ),
-  trials = 50,        # number of hyperparameter combinations tested
+  optimizer = torch::optim_adamw,
+  opt_hparams = list(lr = 5e-04, eps = 1e-08, weight_decay = 7e-04),
+  lr_decay_epochs = 50L,
+  lr_decay_rate = 1,
+  patience = 20L,
+  min_delta = 0.01,
+  seed = NULL,
+  verbose = FALSE,
   multicores = 24,    # parallel execution
   progress = TRUE     # display progress
 )
