75224bcb8b728400f32a2f4c0ae7ea867ecb9d67
Ana carolina <89164595+AnaAndradesant@users.noreply.github.com>
2026-05-11 14:25:28 -0300

    Update 1_mixture_model.R

--- scripts/1_mixture_model.R
+++ scripts/1_mixture_model.R
@@ -7,10 +7,7 @@ library(sits)
 library(tibble)
 
 # Paths for files and folders
-mixture_path <- "data/raw/mixture_model",
-tiles      = c('012014', '012015', '013014', '013015'),
-start_date = '2024-08-01',
-end_date   = '2025-07-31'
+mixture_path <- "data/raw/mixture_model"
 
 # ============================================================
 # 1. Define and Load Raster Data Cubes from a collection
@@ -20,21 +17,23 @@ cube <- sits_cube(
   source     = "BDC",
   collection = "SENTINEL-2-16D",
   bands      = c('B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'CLOUD'),
-  tiles      = tiles,
-  start_date = start_date,
-  end_date   = end_date,
+  tiles      = c('020023', '020024'),
+  start_date = '2022-08-01',
+  end_date   = '2025-07-31',
   progress   = TRUE)
 
 # ============================================================
 # 2. Create Fraction Image Features from the Mixture Model Cube
 # ============================================================
 
-# Step 2.1 -- Define endmember values (class names cannot contain numbers)
+# Step 2.1 -- Define endmember values
 endmembers <- tibble::tribble(
   ~class,    ~B02, ~B03, ~B04, ~B05, ~B06, ~B07, ~B08, ~B8A, ~B11, ~B12,
-  "soil",    1799, 2154, 3028, 3303, 3472, 3656, 3566, 3686, 5097, 4736,
-  "veg",      827,  892,  410, 1070, 4206, 5646, 5495, 6236, 2101,  775,
-  "water",    946,  739,  280,  208,  180,  167,  135,  129,   26,   14,
+  "SOIL",    1799, 2154, 3028, 3303, 3472, 3656, 3566, 3686, 5097, 4736,
+  "VEG",      827,  892,  410, 1070, 4206, 5646, 5495, 6236, 2101,  775,
+  "WATER",    946,  739,  280,  208,  180,  167,  135,  129,   26,   14,
+  "SOILO",    1556, 2291, 5485, 6236, 6889, 7323, 7176, 7530, 10252, 8745,
+  "VEGO",      909,  969,  447, 1126, 4762, 6323, 6193, 6629, 1731,  712,
 )
 
 # Step 2.2 -- Generate the mixture model cube and calculate the process duration
